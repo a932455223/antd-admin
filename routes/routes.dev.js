7fc3162847c4e6be9ab5130589f778bc1fc401a3
simon.du <a932455223@qq.com>
2017-04-13 15:26:22 +0800

    delete unused files in redux

--- routes/routes.dev.js
+++ routes/routes.dev.js
@@ -10,7 +10,7 @@ module.exports = (app, mfs, file) => {
 
   console.log(urlArray);
   app.get([
-    '/','/login', '/customer/*', '/system/*'
+    '/', '/login', '/customer/*', '/system/*'
   ], function(req, res, next) {
     devMiddleWare.waitUntilValid(function() {
       var html = mfs.readFileSync(file);
@@ -20,64 +20,65 @@ module.exports = (app, mfs, file) => {
   });
 
   app.get('/menus/customer', (req, res, next) => {
-    let data = [{
-      id: 1,
-      name: '客户资料',
-      children: [
-        {
-          id: 10,
-          name: '我的客户',
-          url:'/customer/my'
-        }, {
-          id: 11,
-          name: '我关注的客户',
-          url:'/customer/focused'
-        }, {
-          id: 12,
-          name: '公海客户',
-          url:'/customer/undistributed'
-        }, {
-          id: 13,
-          name: '我下属的客户',
-          url:'/customer/subordinate'
-        }, {
-          id: 14,
-          name: '我参与的客户',
-          url:'/customer/participation'
-      },
+    let data = [
       {
-        id: 15,
-        name: '新分配给我的客户',
-        url:'/customer/recentlyDistributed'
-        },
-        {
-          id: 16,
-          name: '我的全部客户',
-          url:'/customer/all'
-        }
-      ]
-    }]
+        id: 1,
+        name: '客户资料',
+        children: [
+          {
+            id: 10,
+            name: '我的客户',
+            url: '/customer/my'
+          }, {
+            id: 11,
+            name: '我关注的客户',
+            url: '/customer/focused'
+          }, {
+            id: 12,
+            name: '公海客户',
+            url: '/customer/undistributed'
+          }, {
+            id: 13,
+            name: '我下属的客户',
+            url: '/customer/subordinate'
+          }, {
+            id: 14,
+            name: '我参与的客户',
+            url: '/customer/participation'
+          }, {
+            id: 15,
+            name: '新分配给我的客户',
+            url: '/customer/recentlyDistributed'
+          }, {
+            id: 16,
+            name: '我的全部客户',
+            url: '/customer/all'
+          }
+        ]
+      }
+    ]
     res.json(data);
     next();
   });
 
-
   app.get('/menus/system', (req, res, next) => {
-    let data = [{
-      id: 2,
-      name: '用户和权限',
-      children: [
-        {
-          id: 20,
-          name: '用户管理',
-          url:'/system/users'
-        }, {
-          id: 21,
-          name: '角色和权限管理',
-          url:'/system/roles'
-        }
-      ]
-    }]
+    let data = [
+      {
+        id: 2,
+        name: '用户和权限',
+        children: [
+          {
+            id: 20,
+            name: '用户管理',
+            url: '/system/users'
+          }, {
+            id: 21,
+            name: '角色和权限管理',
+            url: '/system/roles'
+          }
+        ]
+      }
+    ]
     res.json(data);
     next();
   });
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,12 +5,12 @@
  * 时间： 17.3.2
  */
 
-import React,{Component} from 'react';
-import  './scss/headerStyle.less'
+import React, {Component} from 'react';
+import './scss/headerStyle.less'
 import {connect} from 'react-redux';
-import { Link } from 'react-router';
-import { Menu , Icon, Layout} from 'antd';
-const { Header }  = Layout;
+import {Link} from 'react-router';
+import {Menu, Icon, Layout} from 'antd';
+const {Header} = Layout;
 const SubMenu = Menu.SubMenu;
 const Item = Menu.Item;
 const MenuItemGroup = Menu.ItemGroup;
@@ -20,13 +20,11 @@ class TopHeader extends React.Component {
   };
 
   state = {
-    current: 'mail',
+    current: 'mail'
   }
 
   handleClick = (e) => {
-    this.setState({
-      current: e.key,
-    });
+    this.setState({current: e.key});
   }
 
   render() {
@@ -35,43 +33,39 @@ class TopHeader extends React.Component {
     return (
       <Header className="header">
         <div className="header-wrap">
-            <div className="logo">
-                <img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1490261157755&di=3649d486b6244696b01d3c62a03f1949&imgtype=0&src=http%3A%2F%2Fis3.mzstatic.com%2Fimage%2Fthumb%2FPurple69%2Fv4%2F1b%2F90%2F15%2F1b901510-3952-a3fb-fd46-e8e7a454a7c2%2Fmzl.uunubwhw.png%2F0x0ss-85.jpg" />
-                精准营销系统
-                <div className="version">v1.0</div>
-            </div>
-            <Menu
-                  className="menu"
-                  onClick={this.handleClick}
-                  selectedKeys={[path ? path : this.state.current]}
-                  mode="horizontal">
-              <Menu.Item key="customer">
-                <Link to='/customer/my'>
-                  <Icon type="user" />客户管理
-                </Link>
-              </Menu.Item>
-              <Menu.Item key="workplace">
-                <Link to='/system/users'>
-                 工作台<Icon type="down" />
-                </Link>
-              </Menu.Item>
-              <Menu.Item key="more">
-                <Link to='/system/users'>
-                  更多<Icon type="down" />
-                </Link>
-              </Menu.Item>
-              <Menu.Item key="sound">
-                <Link to='/login'>
-                  <Icon type="logout" />登出
-                </Link>
-              </Menu.Item>
-            </Menu>
+          <div className="logo">
+            <img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1490261157755&di=3649d486b6244696b01d3c62a03f1949&imgtype=0&src=http%3A%2F%2Fis3.mzstatic.com%2Fimage%2Fthumb%2FPurple69%2Fv4%2F1b%2F90%2F15%2F1b901510-3952-a3fb-fd46-e8e7a454a7c2%2Fmzl.uunubwhw.png%2F0x0ss-85.jpg"/>
+            精准营销系统
+            <div className="version">v1.0</div>
+          </div>
+          <Menu className="menu" onClick={this.handleClick} selectedKeys={[path
+              ? path
+              : this.state.current]} mode="horizontal">
+            <Menu.Item key="customer">
+              <Link to='/customer/my'>
+                <Icon type="user"/>客户管理
+              </Link>
+            </Menu.Item>
+            <Menu.Item key="workplace">
+              <Link to='/system/users'>
+                工作台<Icon type="down"/>
+              </Link>
+            </Menu.Item>
+            <Menu.Item key="more">
+              <Link to='/system/users'>
+                更多<Icon type="down"/>
+              </Link>
+            </Menu.Item>
+            <Menu.Item key="sound">
+              <Link to='/login'>
+                <Icon type="logout"/>登出
+              </Link>
+            </Menu.Item>
+          </Menu>
         </div>
       </Header>
     );
   }
 }
 
-
-
-export default TopHeader ;
+export default TopHeader;
