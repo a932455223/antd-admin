4ba22b56cdc8b82b8f1509c08a68196f463fb19c
simon.du <a932455223@qq.com>
2017-04-25 00:44:43 +0800

    update personalBaseInfo

---
+++ src/Pages/Subview/PersonalBasicInfo/widget/MaintainRecord/index.js
@@ -0,0 +1,78 @@
+import React,{Component} from 'react'
+import {Timeline,Button,Icon,Input} from 'antd'
+export  default  class MaintainRecord extends Component {
+  render() {
+    return(
+      <div className="maintainrecord">
+        <Timeline>
+          <Timeline.Item >
+            <div className="timeline">
+              <div className="time">
+                <p>2015-09-01</p>
+                <p>14:02:20</p>
+              </div>
+
+
+              <div className="timelinereacod">
+                <section className="edit">
+                  <span>
+                    <span><Icon type="edit" />编辑</span>
+                    <span><Icon type="delete" />删除</span>
+                  </span>
+                </section>
+
+                <section className="record">
+                  <div>张益达</div>
+                  <span>【电话】</span>进行了
+                  <span>1</span>次
+                  <span>【产品到期提醒】</span>维护
+                </section>
+
+                <section className="text">
+                  <Input  type='textarea'
+                          rows={2}
+                          value='介绍理财产品，制定理财计划书，客户下单'/>
+                  <footer>
+                    <span>取消</span>
+                    <Button>保存</Button>
+                  </footer>
+                </section>
+              </div>
+            </div>
+          </Timeline.Item>
+          <Timeline.Item >
+            <div className="timeline">
+              <div className="time">
+                <p>2015-09-01</p>
+                <p>14:02:20</p>
+              </div>
+
+
+              <div className="timelinereacod">
+                <section className="edit">
+                  <span>
+                    <span><Icon type="edit" />编辑</span>
+                    <span><Icon type="delete" />删除</span>
+                  </span>
+                </section>
+
+                <section className="record">
+                  <div>张益达</div>
+                  <span>【电话】</span>进行了
+                  <span>1</span>次
+                  <span>【产品到期提醒】</span>维护
+                </section>
+
+                <section className="text">
+                  <Input  placeholder='介绍理财产品，制定理财计划书，客户下单'/>
+                </section>
+              </div>
+            </div>
+          </Timeline.Item>
+        </Timeline>
+
+        <Button className="loadmore">加载更多<Icon type="down" /></Button>
+      </div>
+    )
+  }
+}
