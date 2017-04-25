2d02853c4a5bad37f31ccff7d332c120eb42891d
ju1234 <596117054@qq.com>
2017-04-25 21:09:07 +0800

    add Page Product

---
+++ src/Pages/Product/component/FormCreator/index.js
@@ -0,0 +1,121 @@
+/**
+ * 文件说明： 组织机构管理/ form生成器
+ * 详细描述：适用于 列表型
+ * 创建者： JU
+ * 时间： 17.3.2
+ */
+
+
+import React, {Component} from "react";
+import {DatePicker, Form, Input, Select} from "antd";
+
+const FormItem = Form.Item;
+const Option = Select.Option;
+
+export default class FormCreator extends Component {
+  render() {
+    const {getFieldDecorator} = this.props;
+    return (
+      <div>
+        {
+          this.props.items.map((item, index) => {
+            switch (item.type) {
+
+              case 'input':
+                return (
+                  <FormItem
+                    label={<span>{item.label}</span>}
+                    {...item.formItemLayout}
+                    key={item.field}
+                  >
+                    {getFieldDecorator(item.field, {
+                      rules: [{required: item.required, message: item.message}],
+                      initialValue: item.initialValue
+                    })(
+                      <Input />
+                    )}
+                  </FormItem>
+                );
+
+
+              case 'select':
+                return (
+                  <FormItem
+                    label={<span>{item.label}</span>}
+                    {...item.formItemLayout}
+                    key={item.field}
+                  >
+                    {getFieldDecorator(item.field, {
+                      rules: [{required: item.required, message: item.message}],
+                      initialValue: item.initialValue
+                    })(
+                      <Select
+                        getPopupContainer={
+                          () => document.getElementById(this.props.containerID)
+                        }
+                      >
+                        {item.options.map((option, index) => {
+                          return <Option value={option.value} key={item.field + index}>{option.text}</Option>
+                        })}
+                      </Select>
+                    )}
+                  </FormItem>
+                );
+
+              case 'datePicker':
+                return (
+                  <FormItem
+                    label={<span>{item.label}</span>}
+                    {...item.formItemLayout}
+                    key={item.field}
+                  >
+                    {getFieldDecorator(item.field, {
+                      rules: [{required: item.required, message: item.message}],
+                      initialValue: item.initialValue
+                    })(
+                      <DatePicker getCalendarContainer={ () => document.getElementById(this.props.containerID)}/>
+                    )}
+                  </FormItem>
+                );
+
+              case 'textarea':
+                return (
+                  <FormItem
+                    label={<span>{item.label}</span>}
+                    {...item.formItemLayout}
+                    key={item.field}
+                  >
+                    {getFieldDecorator(item.field, {
+                      rules: [{required: item.required, message: item.message}],
+                      initialValue: item.initialValue
+                    })(
+                      <Input type="textarea"/>
+                    )}
+                  </FormItem>
+                );
+
+
+              case 'other':
+                return (
+                  <FormItem
+                    label={<span>{item.label}</span>}
+                    {...item.formItemLayout}
+                    key={item.field}
+                  >
+                    {getFieldDecorator(item.field, {
+                      rules: [{required: item.required, message: item.message}],
+                      initialValue: item.initialValue
+                    })(
+                      item.render
+                    )}
+                  </FormItem>
+                );
+              default:
+                return null;
+            }
+          })
+        }
+      </div>
+    )
+  }
+}
